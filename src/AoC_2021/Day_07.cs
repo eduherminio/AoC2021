9b01fba9b94eec9f450de73cdb40b883b6689239
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2021-12-07 16:41:10 +0100

    Optimized Day 7. Use mean for part 1 and Triangular numbers formula for part 2

--- src/AoC_2021/Day_07.cs
+++ src/AoC_2021/Day_07.cs
@@ -11,9 +11,47 @@ namespace AoC_2021
             _input = ParseInput();
         }
 
-        public override ValueTask<string> Solve_1()
+        public override ValueTask<string> Solve_1() => Solve_1_Mean();
+
+        public override ValueTask<string> Solve_2() => Solve_2_TriangularNumbers();
+
+        private List<int> ParseInput()
+        {
+            var file = new ParsedFile(InputFilePath, new[] { ',' });
+
+            return file.ToList<int>();
+        }
+
+        /// <summary>
+        /// 2-3ms
+        /// </summary>
+        /// <returns></returns>
+        private ValueTask<string> Solve_1_Mean()
+        {
+            _input.Sort();
+            var meanPos = _input.Count / 2.0;
+            var candidate1 = _input[(int)Math.Floor(meanPos)];
+            var candidate2 = _input[(int)Math.Ceiling(meanPos)];
+
+            int fuel1 = 0, fuel2 = 0;
+
+            foreach (var crabPosition in _input)
+            {
+                fuel1 += Math.Abs(crabPosition - candidate1);
+                fuel2 += Math.Abs(crabPosition - candidate2);
+            }
+
+            return new($"{(fuel1 < fuel2 ? fuel1 : fuel2)}");
+        }
+
+        /// <summary>
+        /// 6 ms
+        /// </summary>
+        /// <returns></returns>
+        private ValueTask<string> Solve_1_Iterating()
         {
             var range = RangeHelpers.GenerateRange(_input.Min(), _input.Max());
+
             var fuel = new int[range.Count()];
 
             foreach (var alignPosition in range)
@@ -27,7 +65,32 @@ namespace AoC_2021
             return new($"{fuel.Min()}");
         }
 
-        public override ValueTask<string> Solve_2()
+        /// <summary>
+        /// 6ms, ~part 1 https://en.wikipedia.org/wiki/Triangular_number
+        /// </summary>
+        /// <returns></returns>
+        private ValueTask<string> Solve_2_TriangularNumbers()
+        {
+            var range = RangeHelpers.GenerateRange(_input.Min(), _input.Max());
+            var fuel = new int[range.Count()];
+
+            foreach (var alignPosition in range)
+            {
+                foreach (var crabPosition in _input)
+                {
+                    var dist = Math.Abs(crabPosition - alignPosition);
+                    fuel[alignPosition] += dist * (dist + 1) / 2;
+                }
+            }
+
+            return new($"{fuel.Min()}");
+        }
+
+        /// <summary>
+        /// 2.5s
+        /// </summary>
+        /// <returns></returns>
+        private ValueTask<string> Solve_2_Iterating()
         {
             var range = RangeHelpers.GenerateRange(_input.Min(), _input.Max());
             var fuel = new int[range.Count()];
@@ -44,13 +107,6 @@ namespace AoC_2021
             }
 
             return new($"{fuel.Min()}");
-        }
-
-        private List<int> ParseInput()
-        {
-            var file = new ParsedFile(InputFilePath, new[] { ',' });
-
-            return file.ToList<int>();
         }
     }
 }
