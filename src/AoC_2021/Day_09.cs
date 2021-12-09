8092a5d9e7c59f99484cd8fb00678c794cc1ef25
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2021-12-10 00:32:44 +0100

    Add Day 9 part 1

---
+++ src/AoC_2021/Day_09.cs
@@ -0,0 +1,62 @@
+﻿using SheepTools.Model;
+
+namespace AoC_2021;
+
+public class Day_09 : BaseDay
+{
+    private readonly List<List<int>> _input;
+
+    public Day_09()
+    {
+        _input = ParseInput();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var result = 0;
+        List<(int X, int Y)> lowPoints = new();
+
+        for (int y = 0; y < _input.Count; ++y)
+        {
+            for (int x = 0; x < _input[y].Count; ++x)
+            {
+                var value = _input[y][x];
+
+                for (int y1 = y - 1; y1 <= y + 1; ++y1)
+                {
+                    if (y1 < 0 || y1 > _input.Count - 1)
+                    {
+                        continue;
+                    }
+                    for (int x1 = x - 1; x1 <= x + 1; ++x1)
+                    {
+                        if (x1 < 0 || x1 > _input[y].Count - 1 || (x1 == x && y1 == y))
+                        {
+                            continue;
+                        }
+
+                        if (_input[y1][x1] < value)
+                        {
+                            goto endOfDoubleLoop;
+                        }
+                    }
+                }
+
+                lowPoints.Add((x, y));
+            endOfDoubleLoop:;
+            }
+        }
+
+        return new($"{lowPoints.Sum(point => _input[point.Y][point.X]) + lowPoints.Count}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("");
+    }
+
+    private List<List<int>> ParseInput()
+    {
+        return File.ReadAllLines(InputFilePath).Select(str => str.Select(ch => int.Parse(ch.ToString())).ToList()).ToList();
+    }
+}
--- tests/AoC_2021.Test/SolutionTests.cs
+++ tests/AoC_2021.Test/SolutionTests.cs
@@ -1,31 +1,28 @@
 using AoCHelper;
 using NUnit.Framework;
 
-namespace AoC_2021.Test
+namespace AoC_2021.Test;
+
+public class SolutionTests
 {
-    public static class SolutionTests
+    [TestCase(typeof(Day_01), "1527", "1575")]
+    [TestCase(typeof(Day_02), "1499229", "1340836560")]
+    [TestCase(typeof(Day_03), "3687446", "4406844")]
+    [TestCase(typeof(Day_04), "39984", "8468")]
+    [TestCase(typeof(Day_05), "7438", "21406")]
+    [TestCase(typeof(Day_06), "373378", "1682576647495")]
+    [TestCase(typeof(Day_07), "355989", "102245489")]
+    [TestCase(typeof(Day_09), "423", "")]
+    public async Task Test(Type type, string sol1, string sol2)
     {
-        public class Solutions
+        if (Activator.CreateInstance(type) is BaseProblem instance)
         {
-            [TestCase(typeof(Day_01), "1527", "1575")]
-            [TestCase(typeof(Day_02), "1499229", "1340836560")]
-            [TestCase(typeof(Day_03), "3687446", "4406844")]
-            [TestCase(typeof(Day_04), "39984", "8468")]
-            [TestCase(typeof(Day_05), "7438", "21406")]
-            [TestCase(typeof(Day_06), "373378", "1682576647495")]
-            [TestCase(typeof(Day_07), "355989", "102245489")]
-            public async Task Test(Type type, string sol1, string sol2)
-            {
-                if (Activator.CreateInstance(type) is BaseDay instance)
-                {
-                    Assert.AreEqual(sol1, await instance.Solve_1());
-                    Assert.AreEqual(sol2, await instance.Solve_2());
-                }
-                else
-                {
-                    Assert.Fail();
-                }
-            }
+            Assert.AreEqual(sol1, await instance.Solve_1());
+            Assert.AreEqual(sol2, await instance.Solve_2());
+        }
+        else
+        {
+            Assert.Fail($"{type} is not a BaseDay");
         }
     }
 }
