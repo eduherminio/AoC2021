5fb50c0fb81613b2f00f88168cf63cb342b9d123
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2021-12-15 19:16:49 +0100

    [WIP]

--- src/AoC_2021/Day_14.cs
+++ src/AoC_2021/Day_14.cs
@@ -15,17 +15,54 @@ public class Day_14 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        var result = Mutate(10, _input.Template, _input.Instructions);
+        const int steps = 15;
 
-        return new($"{result.Max.Count - result.Min.Count}");
+        return new($"{Mutate(steps, _input.Template, _input.Instructions)}");
     }
 
-    private static ((char Key, int Count) Max, (char Key, int Count) Min) Mutate(int stepCount, string template, List<(string pattern, string Value)> instructions)
+    public override ValueTask<string> Solve_2()
+    {
+        // var result = Mutate(10, _input.Template, _input.Instructions);
+
+
+        // var other = _input.Instructions.SelectMany(i => i.Value).ToHashSet();
+        // var filteredInstructions = _input.Instructions.Where(i =>
+        //     i.pattern.Contains(result.Max.Key)
+        //     || i.Value.Contains(result.Max.Key)
+        //     || i.pattern.Contains(result.Min.Key)
+        //     || i.Value.Contains(result.Min.Key))
+        //     .ToList();
+
+        // var producers = new HashSet<char>(filteredInstructions.SelectMany(i => i.pattern)){
+        //     result.Max.Key,
+        //     result.Min.Key,
+        // };
+        // var moreFilteredInstructions = _input.Instructions.Where(i => i.Value.Intersect(producers).Count() > 0).ToList();
+
+        // _input.Instructions.RemoveAll(i => new string(i.Value.Intersect(producers).ToArray()) == i.pattern);
+        const int steps = 15;
+
+        return new($"{MutateOptimized(steps, _input.Template, _input.Instructions)}");
+    }
+
+    private (string Template, List<(string pattern, string Value)> Instructions) ParseInput()
+    {
+        var groupsOfLines = ParsedFile.ReadAllGroupsOfLines(InputFilePath);
+        if (groupsOfLines.Count != 2) throw new SolvingException("2 groups of lines expected in input file");
+
+        return (
+            groupsOfLines[0][0],
+            groupsOfLines[1].Select(g => { var parts = g.Split(" -> "); return (parts[0], $"{parts[0][0]}{parts[1]}{parts[0][1]}"); }).ToList()
+        );
+    }
+
+
+    private static long Mutate(int stepCount, string template, List<(string Pattern, string Value)> instructions)
     {
         var result = template;
         for (int step = 0; step < stepCount; ++step)
         {
-            Console.WriteLine($"Step {step + 1}, length {result.Length}");
+            // Console.WriteLine($"Step {step + 1}, length {result.Length}");
             var sb = new StringBuilder();
 
             var prevChar = '\n';
@@ -33,7 +70,7 @@ public class Day_14 : BaseDay
             {
                 var patt = $"{prevChar}{ch}";
 
-                var matching = instructions.FirstOrDefault(i => i.pattern == patt);
+                var matching = instructions.FirstOrDefault(i => i.Pattern == patt);
                 if (matching != default)
                 {
                     sb.Remove(sb.Length - 1, 1);
@@ -51,72 +88,83 @@ public class Day_14 : BaseDay
 
         var groups = result.GroupBy(i => i).OrderByDescending(g => g.Count());
 
-        return (
-            (groups.First().Key, groups.First().Count()),
-            (groups.Last().Key, groups.Last().Count()));
+        return groups.First().Count() - groups.Last().Count();
     }
 
-    public override ValueTask<string> Solve_2()
+    private static long MutateOptimized(int stepCount, string template, List<(string Pattern, string Value)> instructions)
     {
-        var result = Mutate(10, _input.Template, _input.Instructions);
+        var generatorInstructions = instructions.Where(instruction => instruction.Value[1] == instruction.Value[2]).ToList();
+        var nonGeneratorInstructions = instructions.Except(generatorInstructions).ToList();
 
-        var filteredInstructions = _input.Instructions.Where(i =>
-            i.pattern.Contains(result.Max.Key)
-            || i.Value.Contains(result.Max.Key)
-            || i.pattern.Contains(result.Min.Key)
-            || i.Value.Contains(result.Min.Key))
-            .ToList();
+        var allChars = instructions.SelectMany(ins => ins.Value).ToHashSet();
+        var extraCharacterCount = new Dictionary<char, int>(allChars.Select(ch => new KeyValuePair<char, int>(ch, 0)));
 
-        var diff = Mutate(40, _input.Template, filteredInstructions);
-
-        return new($"{diff}");
-    }
-
-    private (string Template, List<(string pattern, string Value)> Instructions) ParseInput()
-    {
-        var groupsOfLines = ParsedFile.ReadAllGroupsOfLines(InputFilePath);
-        if (groupsOfLines.Count != 2) throw new SolvingException("2 groups of lines expected in input file");
-
-        return (
-            groupsOfLines[0][0],
-            groupsOfLines[1].Select(g => { var parts = g.Split(" -> "); return (parts[0], $"{parts[0][0]}{parts[1]}{parts[0][1]}"); }).ToList()
-        );
-    }
-
-    private static HashSet<IntPoint> Fold(HashSet<IntPoint> points, (bool IsX, int Value) foldInstruction)
-    {
-        IEnumerable<IntPoint> pointsToRemove;
-        IEnumerable<IntPoint> pointsToAdd;
-
-        if (foldInstruction.IsX)
+        var result = new StringBuilder(template);
+        for (int step = 0; step < stepCount; ++step)
         {
-            pointsToRemove = points.Where(p => p.X > foldInstruction.Value).ToList();
-            pointsToAdd = pointsToRemove.Select(p => p with { X = foldInstruction.Value + foldInstruction.Value - p.X }).ToList();
-        }
-        else
-        {
-            pointsToRemove = points.Where(p => p.Y > foldInstruction.Value).ToList();
-            pointsToAdd = pointsToRemove.Select(p => p with { Y = foldInstruction.Value + foldInstruction.Value - p.Y }).ToList();
-        }
+            Console.WriteLine($"Step {step + 1}, length {result.Length}");
+            var sb = new StringBuilder();
 
-        foreach (var p in pointsToRemove) points.Remove(p);
-        foreach (var p in pointsToAdd) points.Add(p);
+            var prevChar = '\n';
+            for (int i = 0; i < result.Length; ++i)
+            {
+                var ch = result[i];
+                var patt = $"{prevChar}{ch}";
 
-        return points;
-    }
+                var matching = nonGeneratorInstructions.FirstOrDefault(i => i.Pattern == patt);
+                if (matching != default)
+                {
+                    sb.Remove(sb.Length - 1, 1);
+                    sb.Append(matching.Value);
+                }
+                else
+                {
+                    var generatorMatching = generatorInstructions.FirstOrDefault(i => i.Pattern == patt);
+                    if (generatorMatching == default)
+                    {
+                        sb.Append(ch);
+                    }
+                    else
+                    {
+                        if (i == result.Length - 1 || result[i + 1] != ch)
+                        {
+                            sb.Append(ch);
+                            sb.Append(ch);
+                        }
+                        else
+                        {
+                            sb.Append(ch);
+                            extraCharacterCount[ch]++;
+                        }
+                    }
+                }
+                prevChar = ch;
+            }
 
-    private static string Print(HashSet<IntPoint> points)
-    {
-        var sb = new StringBuilder(Environment.NewLine);
-
-        foreach (var y in RangeHelpers.GenerateRange(points.Min(p => p.Y), points.Max(p => p.Y)))
-        {
-            foreach (var x in RangeHelpers.GenerateRange(points.Min(p => p.X), points.Max(p => p.X)))
-                sb.Append(points.Any(p => p.X == x && p.Y == y) ? 'X' : '.');
-
-            sb.Append(Environment.NewLine);
+            result = sb;
+            if (step < 5)
+                Console.WriteLine(result.ToString());
+            Console.WriteLine(result.Length + extraCharacterCount.Sum(pair => pair.Value));
         }
 
-        return sb.ToString();
+        var groups = result.ToString().GroupBy(i => i);
+
+        var min = long.MaxValue;
+        var max = long.MinValue;
+
+        foreach (var ch in allChars)
+        {
+            var total = groups.First(g => g.Key == ch).Count() + extraCharacterCount[ch];
+            if (total < min)
+            {
+                min = total;
+            }
+            if (total > max)
+            {
+                max = total;
+            }
+        }
+
+        return max - min;
     }
 }
