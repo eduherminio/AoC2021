71bc0785779f53168f8bd821726173945226c07f
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2021-12-16 03:35:13 +0100

    Add Day 15 part 2

--- src/AoC_2021/Day_15.cs
+++ src/AoC_2021/Day_15.cs
@@ -5,15 +5,19 @@ public class Day_15 : BaseDay
     sealed record Node(SheepTools.Model.IntPoint Id, int Risk)
         : SheepTools.Model.GenericNode<SheepTools.Model.IntPoint>(Id)
     {
-        public List<Node> Neighbours { get; private set; }
+        public List<Node> Neighbours { get; private set; } = new List<Node>();
 
-        public Node Parent { get; set; }
+        public int ScoreFromStart { get; set; }
+        public int EstimatedScoreFromStartToEndThroughThis { get; set; }
 
-        public int Depth => Id.Y;
+        private int _manhattanDistance = -1;
+        public int ManhattanDistance => _manhattanDistance;
 
-        public void CalculateNeighbours(List<List<Node>> grid)
+        public void CalculateNeighbours(List<Node> grid)
         {
-            if (Id.Y != grid.Count)
+            var squareSize = (int)(Math.Sqrt(grid.Count));
+
+            if (Id.Y != squareSize)
             {
                 var points = new List<SheepTools.Model.IntPoint>
                 {
@@ -22,11 +26,16 @@ public class Day_15 : BaseDay
                     Id.Move(SheepTools.Model.Direction.Left),
                     Id.Move(SheepTools.Model.Direction.Right)
                 }
-                .Where(p => p.Y >= 0 && p.Y < grid.Count && p.X >= 0 && p.X < grid[p.Y].Count)
+                .Where(p => p.Y >= 0 && p.Y < squareSize && p.X >= 0 && p.X < squareSize)
                 .ToList();
 
-                Neighbours = points.Select(node => grid[node.Y][node.X]).ToList();
+                Neighbours = points.Select(node => grid.First(n => n.Id.X == node.X && n.Id.Y == node.Y)).ToList();
             }
+        }
+
+        public void CalculateManhattanDistance(Node node)
+        {
+            _manhattanDistance = (int)Id.ManhattanDistance(node.Id);
         }
 
         #region Equals override, to avoid taking into account Connections for equality
@@ -38,39 +47,50 @@ public class Day_15 : BaseDay
         #endregion
     }
 
-    private readonly List<List<Node>> _input;
+    private readonly List<Node> _input;
 
     public Day_15()
     {
         _input = ParseInput();
-        foreach (var list in _input)
-        {
-            foreach (var node in list)
-            {
-                node.CalculateNeighbours(_input);
-            }
-        }
     }
 
     public override ValueTask<string> Solve_1()
     {
-        var result = AStarBacktracking(_input);
-
-        return new($"{result}");
+        return new($"{AStarBacktracking(_input)}");
     }
 
     public override ValueTask<string> Solve_2()
     {
+        var squareSize = (int)(Math.Sqrt(_input.Count));
+        var modifiedInput = new List<Node>(_input);
 
-        const int n = 3;
-        var result = 0;
+        foreach (var p in _input)
+        {
+            for (int y = 0; y < 5; ++y)
+            {
+                var risk = Math.DivRem(p.Risk + y, 10, out int remainder) + remainder;
+                for (int x = 0; x < 5; ++x)
+                {
+                    if (x == 0 && y == 0)
+                    {
+                        risk = Math.DivRem(risk + 1, 10, out remainder) + remainder;
+                        continue;
+                    }
+                    modifiedInput.Add(new Node(p.Id with { X = p.Id.X + (x * squareSize), Y = p.Id.Y + (y * squareSize) }, risk));
+                    risk = Math.DivRem(risk + 1, 10, out remainder) + remainder;
+                }
+            }
+        }
 
-        return new("");
+        // Print(modifiedInput);
+
+        var result = AStarBacktracking(modifiedInput);
+        return new($"{result}");
     }
 
-    private List<List<Node>> ParseInput()
+    private List<Node> ParseInput()
     {
-        return File.ReadAllLines(InputFilePath).Select((str, y) => str.Select((ch, x) => new Node(new SheepTools.Model.IntPoint(x, y), int.Parse(ch.ToString()))).ToList()).ToList();
+        return File.ReadAllLines(InputFilePath).SelectMany((str, y) => str.Select((ch, x) => new Node(new SheepTools.Model.IntPoint(x, y), int.Parse(ch.ToString()))).ToList()).ToList();
     }
 
     /// <summary>
@@ -78,47 +98,44 @@ public class Day_15 : BaseDay
     /// </summary>
     /// <param name="allNodes"></param>
     /// <param name="isSuccess"></param>
-    private static int AStarBacktracking(List<List<Node>> allNodes)
+    private static int AStarBacktracking(List<Node> allNodes)
     {
-        var start = allNodes[0][0];
-        var end = allNodes.Last().Last();
+        foreach (var node in allNodes)
+        {
+            node.ScoreFromStart = int.MaxValue;
+            node.EstimatedScoreFromStartToEndThroughThis = int.MaxValue;
+            node.CalculateNeighbours(allNodes);
+            node.CalculateManhattanDistance(allNodes.Last());
+        }
+
+        var start = allNodes[0];
+        var end = allNodes.Last();
 
         var priorityQueue = new PriorityQueue<Node, int>();
-        priorityQueue.Enqueue(start, 0);
 
-        var scores = new Dictionary<Node, int>(allNodes.SelectMany(n => n).Select(n => new KeyValuePair<Node, int>(n, int.MaxValue)))
-        {
-            [start] = 0 // Overridin initial one
-        };
-
-        var finalScores = new Dictionary<Node, int>(allNodes.SelectMany(n => n).Select(n => new KeyValuePair<Node, int>(n, int.MaxValue)))
-        {
-            [start] = 0 // Overridin initial one
-        };
-
-        var cameFrom = new Dictionary<Node, Node>();
+        start.ScoreFromStart = 0;
+        start.EstimatedScoreFromStartToEndThroughThis = 0;
+        priorityQueue.Enqueue(start, start.EstimatedScoreFromStartToEndThroughThis);
 
         while (priorityQueue.TryDequeue(out var current, out _))
         {
             if (current == end)
             {
-                return scores[current];
+                return current.ScoreFromStart;
             }
 
             foreach (var neighbour in current.Neighbours)
             {
-                var tentativeScore = scores[current] + neighbour.Risk;
-                if (tentativeScore < scores[neighbour])
+                var tentativeScore = current.ScoreFromStart + neighbour.Risk;
+                if (tentativeScore < neighbour.ScoreFromStart)
                 {
-                    cameFrom[neighbour] = current;
-                    neighbour.Parent = current;
-                    scores[neighbour] = tentativeScore;
-                    finalScores[neighbour] = tentativeScore;
+                    neighbour.ScoreFromStart = tentativeScore;
+                    neighbour.EstimatedScoreFromStartToEndThroughThis =tentativeScore + neighbour.ManhattanDistance;
 
-                    if (!priorityQueue.UnorderedItems.Any(pair => pair.Element == neighbour))
-                    {
-                        priorityQueue.Enqueue(neighbour, finalScores[neighbour]);
-                    }
+                    // if (!priorityQueue.UnorderedItems.Any(pair => pair.Element == neighbour))
+                    // {
+                    priorityQueue.Enqueue(neighbour, neighbour.EstimatedScoreFromStartToEndThroughThis);
+                    // }
                 }
             }
         }
@@ -126,8 +143,18 @@ public class Day_15 : BaseDay
         throw new SolvingException();
     }
 
-    private static ICollection<Node> ReconstructPath(Node end)
+    private static void Print(List<Node> nodes)
     {
-        throw new NotImplementedException();
+        Node? prevPNode = default;
+        foreach (var p in nodes.OrderBy(p => p.Id.Y).ThenBy(p => p.Id.X))
+        {
+            if (p.Id.Y != prevPNode?.Id.Y)
+            {
+                Console.WriteLine();
+            }
+            Console.Write(p.Risk);
+            prevPNode = p;
+        }
+        Console.WriteLine();
     }
 }
